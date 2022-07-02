70b8df9fed532a59992a991d7608cc7e9184ca61
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-07-02 23:26:05 +0100

    Create useTeamPageData hook

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -1,4 +1,31 @@
 import { useParams } from 'react-router-dom'
+import useTeamNames from '../hooks/useTeamNames'
+import useTeamsArticles from '../hooks/useTeamsArticles'
+import useTeam from '../hooks/useTeam'
+import TeamLogo from './TeamLogo'
+
+function useTeamPageData (teamId) {
+    const {
+        response: teamNames,
+        loading: loadingTeamNames
+    } = useTeamNames()
+
+    const {
+        response: articles,
+        loading: loadingArticles
+    } = useTeamsArticles(teamId)
+    const {
+        response: team,
+        loading: loadingTeam
+    } = useTeam()
+
+    return {
+        teamNames,
+        articles,
+        team,
+        loading: loadingArticles || loadingTeamNames || loadingTeam
+    }
+}
 
 export default function TeamPage () {
     const { teamId } = useParams()
